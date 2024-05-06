520e35d5bb39075002cbc9c8e99211e5be048b7c
s-jad <s_jad@web.de>
2024-05-06 13:38:53 +0200

    Fixed: incorrect use of .filename, setup basic table for notes

--- src/db/init_db.rs
+++ src/db/init_db.rs
@@ -1,16 +1,27 @@
-use sqlx::sqlite::SqlitePool;
-use std::path::PathBuf;
+use sqlx::sqlite::{SqliteConnectOptions, SqlitePool};
 
 use crate::tui::errors::{create_db_error, DbError};
 
-pub(crate) async fn create_db(db_url: PathBuf) -> Result<SqlitePool, DbError> {
-    let db_url_str = db_url
-        .to_str()
-        .ok_or_else(|| create_db_error("Failed to convert db_url to db_url_str".to_string()))?;
+pub(crate) async fn create_db() -> Result<SqlitePool, DbError> {
+    let opts = SqliteConnectOptions::new()
+        .filename("notes.db")
+        .create_if_missing(true);
 
-    let db = SqlitePool::connect(&db_url_str)
+    let conn = SqlitePool::connect_with(opts)
         .await
         .map_err(|e| create_db_error(e.to_string()))?;
 
-    Ok(db)
+    println!("conn: {:?}", conn);
+    let _schema_query = sqlx::query(
+        "CREATE TABLE IF NOT EXISTS notes (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            title TEXT NOT NULL,
+            content TEXT NOT NULL,
+            links TEXT
+        );",
+    )
+    .fetch_optional(&conn)
+    .await?;
+
+    Ok(conn)
 }
