7159717c8dbda802f7df2eca1525ff52e543d63d
s-jad <s_jad@web.de>
2024-05-06 08:01:23 +0200

    Added: TextArea for note taking, AppState enum for finer-grained state control

--- src/tui/app.rs
+++ src/tui/app.rs
@@ -1,28 +1,41 @@
 use color_eyre::eyre::{Context, Result};
-use crossterm::event::{self, Event, KeyCode, KeyEvent, KeyEventKind};
+use crossterm::event::{self, Event, KeyEventKind};
 use ratatui::{
-    layout::Alignment,
-    style::Stylize,
-    symbols::border,
-    text::{Line, Text},
-    widgets::{
-        block::{Position::Bottom, Title},
-        Block, Borders, Paragraph, Widget,
-    },
+    buffer::Buffer,
+    layout::{Alignment, Constraint, Direction, Layout},
+    style::{Modifier, Style},
+    text::Line,
+    widgets::{block::Title, Block, BorderType, Borders, Paragraph, Widget, Wrap},
     Frame,
 };
+use tui_textarea::{Input, Key, TextArea};
 
 use super::utils::Tui;
 
-#[derive(Debug, Default, Clone, Copy)]
-pub(crate) struct App {
-    pub(crate) exit: bool,
+#[derive(PartialEq, Debug, Default, Clone, Copy)]
+pub(crate) enum AppState {
+    #[default]
+    Running,
+    Exit,
 }
 
-impl App {
+#[derive(Debug, Default, Clone)]
+pub(crate) struct App<'a> {
+    pub(crate) state: AppState,
+    pub(crate) note_editor: TextArea<'a>,
+}
+
+impl<'a> App<'a> {
+    pub fn default() -> Self {
+        Self {
+            state: AppState::default(),
+            note_editor: TextArea::default(),
+        }
+    }
     pub fn run(&mut self, terminal: &mut Tui) -> Result<()> {
-        while !self.exit {
-            terminal.draw(|frame| self.render_frame(frame))?;
+        // MAIN PROGRAM LOOP
+        while self.state != AppState::Exit {
+            terminal.draw(|frame| self.clone().render_frame(frame))?;
             self.handle_events().wrap_err("handle events failed")?;
         }
 
@@ -36,44 +49,62 @@ impl App {
     fn handle_events(&mut self) -> Result<()> {
         match event::read()? {
             Event::Key(key_event) if key_event.kind == KeyEventKind::Press => self
-                .handle_key_event(key_event)
+                .handle_key_event(key_event.into())
                 .wrap_err_with(|| format!("handling key event failed:\n{key_event:#?}")),
             _ => Ok(()),
         }
     }
 
-    pub(crate) fn handle_key_event(&mut self, key_event: KeyEvent) -> Result<()> {
-        match key_event.code {
-            KeyCode::Char('q') => self.exit(),
+    pub(crate) fn handle_key_event(&mut self, input: Input) -> Result<()> {
+        match input {
+            Input { key: Key::Esc, .. } => {
+                self.exit();
+            }
             _ => {}
-        }
+        };
         Ok(())
     }
 
     fn exit(&mut self) {
-        self.exit = true;
+        self.state = AppState::Exit;
     }
 }
 
-impl Widget for App {
-    fn render(self, area: ratatui::prelude::Rect, buf: &mut ratatui::prelude::Buffer)
+impl<'a> Widget for App<'a> {
+    fn render(mut self, area: ratatui::prelude::Rect, buf: &mut Buffer)
     where
         Self: Sized,
     {
-        let title = Title::from(" Counter App Tutorial ".bold());
-        let instructions = Title::from(Line::from(vec![" Quit ".into(), "<Q> ".blue().bold()]));
+        let layout = Layout::default()
+            .direction(Direction::Horizontal)
+            .constraints(&[Constraint::Percentage(80), Constraint::Percentage(20)])
+            .split(area);
 
-        let block = Block::default()
-            .title(title.alignment(Alignment::Center))
-            .title(instructions.alignment(Alignment::Center).position(Bottom))
-            .borders(Borders::all())
-            .border_set(border::THICK);
+        let note_block = Block::default()
+            .title(Title::from("Note Editor").alignment(Alignment::Center))
+            .title_style(Style::default().add_modifier(Modifier::BOLD))
+            .borders(Borders::ALL)
+            .border_type(BorderType::Thick)
+            .title_bottom(Line::from(" <Esc> Quit "));
 
-        let welcome_txt = Text::from(vec![Line::from(vec!["Welcome to tuipaz!".into()])]);
+        self.note_editor.set_block(note_block);
 
-        Paragraph::new(welcome_txt)
-            .centered()
-            .block(block)
-            .render(area, buf);
+        let note_editor_widget = self.note_editor.widget();
+
+        note_editor_widget.render(layout[0], buf);
+
+        let files_block = Block::default()
+            .title(Title::from("File Explorer").alignment(Alignment::Center))
+            .title_style(Style::default().add_modifier(Modifier::BOLD))
+            .borders(Borders::ALL)
+            .border_type(BorderType::Thick);
+
+        let files_text =
+            vec![Line::from("This is the file explorer sidebar").style(Style::default())];
+
+        Paragraph::new(files_text)
+            .block(files_block)
+            .wrap(Wrap { trim: true })
+            .render(layout[1], buf);
     }
 }
