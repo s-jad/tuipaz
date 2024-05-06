a46cdfae953249d9271fb96dca4729da2c149e1b
s-jad <s_jad@web.de>
2024-05-06 10:32:09 +0200

    Added dotenv for db url, init_db fn

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,16 @@
+mod db;
 mod tui;
 
 use color_eyre::Result;
+use db::init_db;
+use dotenv::dotenv;
 use tui::app::App;
 
-fn main() -> Result<()> {
+#[tokio::main]
+async fn main() -> Result<()> {
+    dotenv().ok();
+    let db_url = dotenv()?;
+    init_db::create_db(db_url).await?;
     tui::errors::install_hooks()?;
     let mut term = tui::utils::init()?;
     let res = App::default().run(&mut term)?;
