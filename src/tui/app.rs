fa698c2d1fa8cfdcc236c7b63408028e1ff7e6d1
s-jad <s_jad@web.de>
2024-05-07 17:50:41 +0200

    Refactor: split control into AppMac, App now only contains state

--- src/tui/app.rs
+++ src/tui/app.rs
@@ -1,19 +1,15 @@
 use color_eyre::eyre::{Context, Result};
-use crossterm::event::{self, Event, KeyEventKind};
 use ratatui::{
-    buffer::Buffer,
     layout::{Alignment, Constraint, Direction, Layout},
-    style::{Modifier, Style},
-    text::Line,
+    style::{Color, Modifier, Style},
+    text::{Line, Text},
     widgets::{block::Title, Block, BorderType, Borders, Paragraph, Widget, Wrap},
     Frame,
 };
 use sqlx::{Pool, Sqlite};
-use tui_textarea::{Input, Key, TextArea};
+use tui_textarea::TextArea;
 
-use crate::db::db_mac::NotesMac;
-
-use super::{user_messages::UserMessage, utils::Tui};
+use super::{events::AppMac, user_messages::centered_rect, utils::Tui};
 
 #[derive(PartialEq, Debug, Default, Clone, Copy)]
 pub(crate) enum AppState {
@@ -22,115 +18,105 @@ pub(crate) enum AppState {
     Exit,
 }
 
+#[derive(PartialEq, Debug, Clone, Copy)]
+pub(crate) enum CurrentScreen {
+    Main,
+    Popup,
+    Exiting,
+}
+
 #[derive(Debug, Clone)]
 pub(crate) struct App<'a> {
     pub(crate) state: AppState,
+    pub(crate) screen: CurrentScreen,
+    pub(crate) editor: TextArea<'a>,
     pub(crate) db: Pool<Sqlite>,
-    pub(crate) note_editor: TextArea<'a>,
-    pub(crate) user_message: UserMessage,
 }
 
 impl<'a> App<'a> {
     pub fn new(db: Pool<Sqlite>) -> Self {
         Self {
             state: AppState::default(),
+            screen: CurrentScreen::Main,
+            editor: TextArea::default(),
             db,
-            note_editor: TextArea::default(),
-            user_message: UserMessage::welcome(),
         }
-    }
-    pub async fn run(&mut self, terminal: &mut Tui) -> Result<()> {
-        // MAIN PROGRAM LOOP
-        while self.state != AppState::Exit {
-            terminal.draw(|frame| self.clone().render_frame(frame))?;
-            let result = self.handle_events().await;
-
-            result.wrap_err("handle events failed")?;
-        }
-
-        Ok(())
-    }
-
-    fn render_frame(self, frame: &mut Frame) {
-        frame.render_widget(self, frame.size());
-    }
-
-    async fn handle_events(&mut self) -> Result<()> {
-        match event::read()? {
-            Event::Key(key_event) if key_event.kind == KeyEventKind::Press => {
-                let result = self.handle_key_event(key_event.into()).await;
-                result.wrap_err_with(|| format!("handling key event failed:\n{key_event:#?}"))
-            }
-            _ => Ok(()),
-        }
-    }
-
-    async fn handle_key_event(&mut self, input: Input) -> Result<()> {
-        match input {
-            Input { key: Key::Esc, .. } => {
-                self.exit();
-            }
-            Input {
-                key: Key::Char('s'),
-                alt: true,
-                ..
-            } => {
-                self.save_note().await?;
-            }
-            input => {
-                self.note_editor.input(input);
-            }
-        };
-        Ok(())
-    }
-
-    async fn save_note(&self) -> Result<()> {
-        let note = self.note_editor.lines().join("\n");
-        let result = NotesMac::save_note(&self.db, note).await;
-
-        return result;
-    }
-
-    fn exit(&mut self) {
-        self.state = AppState::Exit;
     }
 }
 
-impl<'a> Widget for App<'a> {
-    fn render(mut self, area: ratatui::prelude::Rect, buf: &mut Buffer)
-    where
-        Self: Sized,
-    {
-        let layout = Layout::default()
-            .direction(Direction::Horizontal)
-            .constraints(&[Constraint::Percentage(80), Constraint::Percentage(20)])
-            .split(area);
+pub(crate) async fn run(app: &mut App<'_>, terminal: &mut Tui) -> Result<()> {
+    // MAIN PROGRAM LOOP
+    while app.state != AppState::Exit {
+        terminal.draw(|frame| ui(app, frame))?;
+        let result = AppMac::handle_events(app).await;
 
-        let note_block = Block::default()
-            .title(Title::from("Note Editor").alignment(Alignment::Center))
-            .title_style(Style::default().add_modifier(Modifier::BOLD))
-            .borders(Borders::ALL)
-            .border_type(BorderType::Thick)
-            .title_bottom(Line::from(" <Esc> Quit "));
-
-        self.note_editor.set_block(note_block);
-
-        let note_editor_widget = self.note_editor.widget();
-
-        note_editor_widget.render(layout[0], buf);
-
-        let files_block = Block::default()
-            .title(Title::from("File Explorer").alignment(Alignment::Center))
-            .title_style(Style::default().add_modifier(Modifier::BOLD))
-            .borders(Borders::ALL)
-            .border_type(BorderType::Thick);
-
-        let files_text =
-            vec![Line::from("This is the file explorer sidebar").style(Style::default())];
-
-        Paragraph::new(files_text)
-            .block(files_block)
-            .wrap(Wrap { trim: true })
-            .render(layout[1], buf);
+        result.wrap_err("handle events failed")?;
     }
+
+    Ok(())
+}
+
+fn ui(app: &mut App, frame: &mut Frame) {
+    let area = frame.size();
+    match app.screen {
+        CurrentScreen::Main => render_main_screen(app, frame),
+        CurrentScreen::Popup => render_popup(app, frame),
+        CurrentScreen::Exiting => render_exit_screen(frame),
+    }
+}
+
+fn render_main_screen<'a>(app: &mut App, frame: &mut Frame) {
+    let area = frame.size();
+    let buf = frame.buffer_mut();
+
+    let layout = Layout::default()
+        .direction(Direction::Horizontal)
+        .constraints(&[Constraint::Percentage(80), Constraint::Percentage(20)])
+        .split(area);
+
+    let editor_block = Block::default()
+        .title(Title::from("Note Editor").alignment(Alignment::Center))
+        .title_style(Style::default().add_modifier(Modifier::BOLD))
+        .borders(Borders::ALL)
+        .border_type(BorderType::Thick)
+        .title_bottom(Line::from(" <Esc> Quit "));
+
+    app.editor.set_block(editor_block);
+    let editor_widget = app.editor.widget();
+
+    editor_widget.render(layout[0], buf);
+
+    let files_block = Block::default()
+        .title(Title::from("File Explorer").alignment(Alignment::Center))
+        .title_style(Style::default().add_modifier(Modifier::BOLD))
+        .borders(Borders::ALL)
+        .border_type(BorderType::Thick);
+
+    let files_text = vec![Line::from("This is the file explorer sidebar").style(Style::default())];
+
+    Paragraph::new(files_text)
+        .block(files_block)
+        .wrap(Wrap { trim: true })
+        .render(layout[1], buf);
+}
+
+fn render_popup<'a>(app: &mut App<'a>, frame: &mut Frame) {
+    //   app.user_msg.clone().render(area, buf);
+}
+
+fn render_exit_screen<'a>(frame: &mut Frame) {
+    let area = frame.size();
+    let buf = frame.buffer_mut();
+
+    let popup_block = Block::default()
+        .title("Y/N")
+        .borders(Borders::NONE)
+        .style(Style::default().bg(Color::DarkGray));
+
+    let exit_text = Text::styled("Exit Tuipaz? (y/n)", Style::default().fg(Color::Red));
+    // the `trim: false` will stop the text from being cut off when over the edge of the block
+    Paragraph::new(exit_text)
+        .block(popup_block)
+        .wrap(Wrap { trim: false })
+        .render(centered_rect(60, 25, area), buf);
 }
