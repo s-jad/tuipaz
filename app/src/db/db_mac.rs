dcad458ac149c810caf1ebdfb3b12e4722bd0e57
s-jad <s_jad@web.de>
2024-05-13 08:10:15 +0200

    Added: update_note fn, Refactor: NoteTitle -> NoteIdentifier, includes id so title can be changed by user and search still works

--- app/src/db/db_mac.rs
+++ app/src/db/db_mac.rs
@@ -12,11 +12,12 @@ use sqlx::SqlitePool;
     Serialize,
     Deserialize,
 )]
-pub(crate) struct NoteTitle {
+pub(crate) struct NoteIdentifier {
+    pub(crate) id: i64,
     pub(crate) title: String,
 }
 
-impl ToString for NoteTitle {
+impl ToString for NoteIdentifier {
     fn to_string(&self) -> String {
         self.title.clone()
     }
@@ -59,12 +60,33 @@ impl DbMac {
         }
     }
 
-    pub(crate) async fn load_note(db: &SqlitePool, title: &str) -> Result<Note> {
+    pub(crate) async fn update_note(
+        db: &SqlitePool,
+        title: String,
+        body: String,
+        id: i64,
+    ) -> Result<()> {
+        let result = sqlx::query!(
+            "UPDATE notes SET title=?, body=? WHERE id=?",
+            title,
+            body,
+            id
+        )
+        .execute(db)
+        .await;
+
+        match result {
+            Ok(_) => Ok(()),
+            Err(e) => Err(eyre!("Failed to save note: {:?}", e)),
+        }
+    }
+
+    pub(crate) async fn load_note(db: &SqlitePool, id: i64) -> Result<Note> {
         //panic!("title: {title:?}");
         let result = sqlx::query_as!(
             Note,
-            "SELECT id, title, COALESCE(body, '') AS body, COALESCE(links, '') AS links FROM notes WHERE title=? ",
-            title
+            "SELECT id, title, COALESCE(body, '') AS body, COALESCE(links, '') AS links FROM notes WHERE id=? ",
+            id
         )
         .fetch_one(db)
         .await;
@@ -77,14 +99,14 @@ impl DbMac {
         }
     }
 
-    pub(crate) async fn load_note_titles(db: &SqlitePool) -> Result<Vec<NoteTitle>> {
-        let result = sqlx::query_as!(NoteTitle, "SELECT title FROM notes")
+    pub(crate) async fn load_note_identifiers(db: &SqlitePool) -> Result<Vec<NoteIdentifier>> {
+        let result = sqlx::query_as!(NoteIdentifier, "SELECT id, title FROM notes")
             .fetch_all(db)
             .await;
 
         match result {
             Ok(notes) => Ok(notes),
-            Err(e) => Err(eyre!("Failed to load note titles: {:?}", e)),
+            Err(e) => Err(eyre!("Failed to load note identifiers: {:?}", e)),
         }
     }
 }
