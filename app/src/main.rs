e53d710bec3b46133217402b99f4d0b5f1997b96
s-jad <s_jad@web.de>
2024-05-23 15:54:30 +0200

    Added: logging for debugging

--- app/src/main.rs
+++ app/src/main.rs
@@ -1,6 +1,12 @@
 mod db;
 mod tui;
 
+use std::path::PathBuf;
+
+use log::LevelFilter;
+use log4rs::config::{Config, Root, Appender};
+use log4rs::encode::pattern::PatternEncoder;
+use log4rs::append::file::FileAppender;
 use color_eyre::Result;
 use db::{db_mac::DbMac, init_db};
 use dotenv::dotenv;
@@ -9,6 +15,22 @@ use tui::app::{run, App};
 #[tokio::main]
 async fn main() -> Result<()> {
     dotenv().ok();
+    
+    // Initialize logging
+    let path = PathBuf::from("./logs/application.log");
+    match FileAppender::builder()
+       .encoder(Box::new(PatternEncoder::new("{d(%Y-%m-%d %H:%M:%S)} | {({l}):5.5} | {f}:{L}{n}— {m}{n}")))
+       .build(path) {
+        Ok(file_appender) => {
+            let config = Config::builder()
+               .appender(Appender::builder().build("file", Box::new(file_appender))) // Now correctly passing a Box<dyn Append>
+               .build(Root::builder().appender("file").build(LevelFilter::Info))?;
+
+            log4rs::init_config(config).expect("Failed to initialize logger");
+        },
+        Err(e) => eprintln!("Failed to create file appender: {}", e),
+    }
+
     tui::errors::install_hooks()?;
     let db = init_db::create_db().await?;
     let mut term = tui::utils::init()?;
