f7a7e0c0f19d6a98c96006f22274153be72ebdcb
s-jad <s_jad@web.de>
2024-06-23 15:10:31 +0200

    Added: load_config func, return defaults if user hasnt chosen option in .toml file

--- app/src/tui/config.rs
+++ app/src/tui/config.rs
@@ -1,3 +1,7 @@
+use std::fs;
+
+use serde::Deserialize;
+
 #[derive(Debug, Clone)]
 pub(crate) struct ColorScheme {
     pub(crate) title: String,
@@ -12,7 +16,7 @@ pub(crate) struct ColorScheme {
     pub(crate) search: String,
 }
 
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, Deserialize)]
 pub(crate) struct UserColorScheme {
     pub(crate) title: Option<String>,
     pub(crate) text: Option<String>,
@@ -26,7 +30,46 @@ pub(crate) struct UserColorScheme {
     pub(crate) search: Option<String>,
 }
 
+#[derive(Debug, Clone)]
+pub(crate) struct Config {
+    pub(crate) colors: ColorScheme,
+}
+
+impl Config {
+    fn new(colors: ColorScheme) -> Self {
+        Self {
+            colors,
+        }
+    }
+}
+
 impl ColorScheme {
+    fn new(
+        title: String,
+        text: String,
+        links: String,
+        normal_mode: String,
+        insert_mode: String,
+        visual_mode: String,
+        search_mode: String,
+        boundaries: String,
+        select: String,
+        search: String,
+    ) -> Self {
+        Self {
+            title,
+            text,
+            links,
+            normal_mode,
+            insert_mode,
+            visual_mode,
+            search_mode,
+            boundaries,
+            select,
+            search,
+        }
+    }
+
     fn default() -> Self {
         Self {
             title: "purple".to_string(),
@@ -40,5 +83,34 @@ impl ColorScheme {
             select: "purple".to_string(),
             search: "red".to_string(),
         }
+    }
+}
+
+fn load_config(path: &str) -> Result<Config, Box<dyn std::error::Error>> {
+    let default_cs = ColorScheme::default();
+
+    if fs::metadata(path).is_ok() {
+        let content = fs::read_to_string(path)?;
+        let user_cs: UserColorScheme = toml::from_str(&content)?;
+        
+        let cs = ColorScheme::new(
+            user_cs.title.unwrap_or(default_cs.title), 
+            user_cs.text.unwrap_or(default_cs.text),
+            user_cs.links.unwrap_or(default_cs.links),
+            user_cs.normal_mode.unwrap_or(default_cs.normal_mode),
+            user_cs.insert_mode.unwrap_or(default_cs.insert_mode),
+            user_cs.visual_mode.unwrap_or(default_cs.visual_mode),
+            user_cs.search_mode.unwrap_or(default_cs.search_mode),
+            user_cs.boundaries.unwrap_or(default_cs.boundaries),
+            user_cs.select.unwrap_or(default_cs.select),
+            user_cs.search.unwrap_or(default_cs.search),
+        );
+
+        let cfg = Config::new(cs);
+
+        Ok(cfg)
+    } else {
+        let default_cfg = Config::new(default_cs);
+        Ok(default_cfg)
     }
 }
